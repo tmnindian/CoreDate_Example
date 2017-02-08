9c452e7d2bdedaf0151f922520c1f2689426476d
Naveen1995 <naveenkumar.thangavel@ionixxtech.com>
2017-02-08 18:08:44 +0530

    Created

---
+++ CoreDate_New/Table_Cell.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  CoreDate_New
+//
+//  Created by Naveen on 08/02/17.
+//  Copyright © 2017 myTest. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Table_Cell : UITableViewCell{
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
