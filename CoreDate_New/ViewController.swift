4ee32cf494137be99eb03db454193c8c0f86d695
Naveen1995 <naveenkumar.thangavel@ionixxtech.com>
2017-02-08 12:23:35 +0530

    Initial Commit

---
+++ CoreDate_New/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDate_New
+//
+//  Created by Naveen on 08/02/17.
+//  Copyright © 2017 myTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
